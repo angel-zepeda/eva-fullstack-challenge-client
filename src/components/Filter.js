3dc492297e2470f07196747b6156c9166d185963
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-12-22 17:35:37 -0600

    Add checkbox for strict or lax mode

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,23 +4,39 @@ import { SERVER } from '../consts';
 
 const Filter = ({ setExplorations }) => {
 
-  const [clinic, setClinic] = useState('');
+  let page = window.location.search.split('&')[0].split('=')[1];
+  let limit = window.location.search.split('&')[1].split('=')[1];
+
+  const [search, setSearch] = useState({
+    clinic: '',
+    medications: '',
+    strict: false
+  });
 
   const filterForm = e => {
-    let page = window.location.search.split('&')[0].split('=')[1];
-    let limit = window.location.search.split('&')[1].split('=')[1];
+    setSearch({ ...search, [e.target.name]: e.target.value })
     if (e.target.value === "") return window.location.reload(true);
-    getExplorationsByClinic(e.target.value, page, limit);
   }
 
-  const getExplorationsByClinic = async (clinic, page, limit) => {
-    const response = await axios.get(SERVER + `/explorations/search/?clinic=${clinic}&page=${page}&limit=${limit}`)
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(search)
+    getExplorationsByClinic(search.clinic, search.medications, page, limit, search.strict)
+  }
+
+  const getExplorationsByClinic = async (clinic, medications, page, limit, mode) => {
+    const response = await axios.get(SERVER + `/explorations/search/?clinic=${clinic}&medications=${medications}&page=${page}&limit=${limit}&strict=${mode}`)
+    console.log(response);
     setExplorations(response.data.data);
   }
 
   return (
-    <form className="form-group row">
+    <form
+      onSubmit={handleSubmit}
+      className="form-group row"
+    >
       <select
+        name="clinic"
         onChange={filterForm}
         className="form-control col-md-3"
       >
@@ -31,9 +47,18 @@ const Filter = ({ setExplorations }) => {
         <option value="EXPLANADA">EXPLANADA</option>
       </select>
       <input
+        onChange={filterForm}
+        name="medications"
         placeholder="Medicaciones"
         className="form-control col-md-3 ml-2"
       />
+      <div className="form-check m-2">
+        <label className="form-check-label">
+          <input className="form-check-input" name="strict" onChange={filterForm} type="checkbox" value={!search.strict} />
+          <span className="text-info">Default: LAX</span>
+        </label>
+      </div>
+
       <button
         className="btn btn-primary btn-sm p-2 ml-2"
       >
