1e77679f70b5becc4f9839e3ce167719d9c286f2
Angel de Jesus Zepeda Torres <angel.zepeda0508@hotmail.com>
2019-12-23 11:58:07 -0600

    Add redirect in register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,27 +2,41 @@ import React, { useState } from 'react';
 import Spinner from '../Spinner';
 import { API_URL } from '../../consts';
 import axios from 'axios';
+import Notice from '../Notice';
 
-const Register = () => {
-
+const Register = ({ history }) => {
+  const [error, setError] = useState('');
   const [user, setUser] = useState({ email: '', password: '' });
   const [showSpinner, setShowSpinner] = useState(false);
   const handleChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
   const handleOnSubmit = e => {
     e.preventDefault();
+    setShowSpinner(true);
     register();
   }
 
   const register = async () => {
     const response = await axios.post(API_URL + '/register', user);
-    setShowSpinner(false);
-    console.log(response);
+    console.log(response.data.code)
+    if (response.data.code === 200) {
+      setShowSpinner(false);
+      history.push('/')
+    }
+    if (response.data.code === 404) {
+      setShowSpinner(false);
+      setError(response.data.message);
+    }
+
   }
 
 
   return (
     <div>
+      {error ?
+        <Notice type="danger" message={error} />
+        : null
+      }
       <div className="col-md-4 col-sm-12 mx-auto card mt-5 p-4">
         <h2 className="text-center">Crear cuenta</h2>
         <form onSubmit={handleOnSubmit} className="form-group card-body p-2">
--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://192.168.1.75:5000/api/v1/";
+export const API_URL = "http://localhost:5000/api/v1/";
